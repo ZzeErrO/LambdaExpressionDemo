394e626c64a54a84a8956e9807392d58d926cfd7
prashikjaware <prashikjaware123@gmail.com>
2021-03-20 11:03:40 +0530

    [Prashik]Added: Create Person Class with SSN, Name, Address and Age fields

---
+++ LambdaDemoSolution/LambdaDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressionsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> listPersonInCity = new List<Person>();
+            //AddRecords(listPersonInCity);
+            //Retriving_TopTwoRecord_ForAges_LessThanSixty(listPersonInCity);
+            //CheckingForTeenagesPerson(listPersonInCity);
+            Console.ReadKey();
+        }
+    }
+}
